2ba0efaaf49af0855f1b752803c63258b3761445
abs <abs@pineappledevelopers.com>
2019-04-23 10:08:52 +0200

    ny metode - study field

--- UCL-InternationalStudentManagement/StudyField/StudyField.cs
+++ UCL-InternationalStudentManagement/StudyField/StudyField.cs
@@ -18,5 +18,11 @@ namespace StudyField
         {
             return null;
         }
+
+        //Creates and passes new field study name to data access layer to create new entity in database
+        public void CreateNewStudyField(string _fieldName)
+        {
+            //Missing class for connect to database, maybe we will decide on something ...?
+        }
     }
 }
